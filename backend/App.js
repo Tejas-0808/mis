3c2d4ca4d2f1dd715340e7dcad09afde4569c787
abdulwaseiitb <76050401+abdulwaseiitb@users.noreply.github.com>
2023-02-10 21:49:26 +0530

    abc.js

--- backend/App.js
+++ backend/App.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(require('./router/auth'));
 
 app.get('/',(req,res) => {
-    res.send(`Hello world from home`);
+    res.send(`Hello worldddd`);
 });
 
 app.listen(port || process.env.port, ()=> {
