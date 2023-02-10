ac78be4ee809d51a7d494ef8bfe5877fde6d6732
faiza4170 <faiza.thedreamer@gmail.com>
2023-02-10 21:58:16 +0530

    abcdef.js

--- backend/App.js
+++ backend/App.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(require('./router/auth'));
 
 app.get('/',(req,res) => {
-    res.send(`Hello worldddd abc`);
+    res.send(`Hello worldddd abc returns`);
 });
 
 app.listen(port || process.env.port, ()=> {
