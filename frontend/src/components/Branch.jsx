98491d55bfc64a97721ec3e3945371551ea5ca5a
RahulNimkande <rknimkande1781@gmail.com>
2023-02-15 14:41:22 +0530

    branch delete

--- frontend/src/components/Branch.jsx
+++ frontend/src/components/Branch.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Branch = () => {
 
@@ -21,7 +22,22 @@ const Branch = () => {
     fetchAllBranch();
     // eslint-disable-next-line
   }, []);
+  const navigate = useNavigate();
 
+  
+  const handleDelete= async (id) =>{
+    try{
+      console.log(id)
+      await axios.delete("http://localhost:3001/branch/"+id)
+      const res = await axios.get("http://localhost:3001/branch");
+        setBranch(res.data);
+      // window.location.reload()
+      // navigate("/");
+    }catch(err){
+      console.log(err);
+    }
+
+  }
   console.log(branch);
 
   return (
@@ -29,9 +45,20 @@ const Branch = () => {
         <h1>
             branch info
         </h1>
+        <div className="branch">
+        {branch.map((branch) => (
+          <div key={branch.Branch_id} className="brach">
+            <h2>{branch.Branch_id}</h2>
+            <p>{branch.Branch_name}</p>
+            <p>{branch.HOD}</p>
+            <p>{branch.Students_enrolled}</p>
+            <button className="delete" onClick={()=>handleDelete(branch.Branch_id)}>Delete</button>
+            </div>
+        ))}
         <button>
             <Link to="/add">Add new Branch</Link>
         </button>
+    </div>
     </div>
   )
 }
