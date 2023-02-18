b850cb31dbec64ed007d379b905a5aa3555baec0
Drag0A <amankushwaha692@gmail.com>
2023-02-18 13:34:53 +0530

    minor change

---
+++ frontend/src/components/Student/Coursereg/Courseregactivity.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom";
+
+
+const CourseRegActivity () => {
+    return (<div>
+        </div>)
+}
+
+export default CourseRegActivity;
