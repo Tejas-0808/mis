e4b2d536e518459a90d89b8ab2367379393b35e2
RahulNimkande <rknimkande1781@gmail.com>
2023-02-20 14:05:34 +0530

    minor change home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,12 +4,36 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div>
+      <p>
         <button>
             <Link to="/branch">Branch</Link>
         </button>
+      </p>
+      <p>
         <button>
             <Link to="/student">Student</Link>
         </button>
+      </p>
+      <p>
+        <button>
+            <Link to="/caste">Caste</Link>
+        </button>
+      </p>
+      <p>
+        <button>
+            <Link to="/category">Category</Link>
+        </button>
+      </p>
+      <p>
+        <button>
+            <Link to="/city">City</Link>
+        </button>
+      </p>
+      <p>
+        <button>
+            <Link to="/state">State.</Link>
+        </button>
+      </p>
     </div>
   )
 }
