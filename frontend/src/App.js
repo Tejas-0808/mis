069ed2094a12c499513242f24eb50128889ffe7a
griya0408 <riya.gangapurwala@gmail.com>
2023-02-21 14:09:38 +0530

    add personal details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,10 +10,12 @@ import Branch from "./components/Branch";
 import Student from "./components/Student";
 //import Staff from "./components/Staff";
 import Add from "./components/Add";
-import City from "./components/Studentsection/Academic/Masters/City"
-import Addcity from "./components/Studentsection/Academic/Masters/Addcity"
+// import City from "./components/Studentsection/Academic/Masters/City";
+// import Addcity from "./components/Studentsection/Academic/Masters/Addcity";
 import State from "./components/Studentsection/Academic/Masters/State";
 import AddState from "./components/Studentsection/Academic/Masters/AddState";
+import Add_per_d from "./components/Student/Profile/AddPersonalDetails";
+import PersonalDetails from "./components/Student/Profile/Personaldetails";
 
 function App() {
   return (
@@ -23,10 +25,12 @@ function App() {
           <Route path="/" element={<Branch/>}/>
           <Route path="/add" element={<Add/>}/>
           <Route path="/student" element={<Student/>}/>
-          <Route path="/city" element={<City/>}/>
-          <Route path="/addcity" element={<Addcity/>}/>
+          {/* <Route path="/city" element={<City/>}/>
+          <Route path="/addcity" element={<Addcity/>}/> */}
           <Route path="/state" element={<State/>}/>
           <Route path="/addstate" element={<AddState/>}/>
+          <Route path="/addpersonaldetails" element={<Add_per_d/>}/>
+          <Route path="/personaldetails" element={<PersonalDetails/>}/>
 
         </Routes>
       </BrowserRouter>
