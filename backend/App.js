89112d95b155feab1c2db3383ec38bcda613c211
faiza4170 <faiza.thedreamer@gmail.com>
2023-02-28 13:48:01 +0530

    rolllist

--- backend/App.js
+++ backend/App.js
@@ -45,7 +45,7 @@ app.use(require('./router/citylist'));
 app.use(require('./router/state'));
 app.use(require('./router/particularstudent'));
 app.use(require('./router/courses_taken'));
-
+app.use(require('./router/rolllist'));
 
 app.get('/',(req,res) => {
     res.send(`Hello worldddd abc returns one`);
