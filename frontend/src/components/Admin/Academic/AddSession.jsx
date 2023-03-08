e78d712f608044772ca67e5989be0b1bd4db177e
faiza4170 <faiza.thedreamer@gmail.com>
2023-03-09 01:37:21 +0530

    frontend addscheme

--- frontend/src/components/Admin/Academic/AddSession.jsx
+++ frontend/src/components/Admin/Academic/AddSession.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+//import { useState, } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from "react-router-dom";
 import { Box, Button } from "@mui/material";
