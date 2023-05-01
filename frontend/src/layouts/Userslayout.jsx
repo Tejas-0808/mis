f281aeb5f1e2049a0050d21b821de62fdf193e4e
Rutuja Gosavi <gosavirutu523@gmail.com>
2023-05-01 10:11:30 +0530

    check add caste

--- frontend/src/layouts/Userslayout.jsx
+++ frontend/src/layouts/Userslayout.jsx
@@ -1,23 +1,34 @@
 import { Routes, Route } from "react-router-dom";
 import SideBar from "../components/Student/sidebar";
+import Schemeallotment from "../components/Users/Academic/Studentsectiontransaction/Schemeallotment";
+import Facultyadvisor from "../components/Users/Academic/Studentsectiontransaction/Facultyadvisor";
+import Promotion from "../components/Users/Academic/Studentsectiontransaction/Promotion";
+import UserDashboard from "../components/Users/UserDashboard";
+import Offeredcourses from "../components/Users/Academic/Studentsectiontransaction/Offeredcourses";
+import FinalCoursesOffered from "../components/Student/Coursereg/FinalCoursesOffered";
+import NewUser from "../components/Users/HOD/NewUser";
+import Batchallotment from "../components/Users/Academic/Studentsectiontransaction/Batchallotment";
+import Courseallotment from "../components/Users/Academic/Studentsectiontransaction/Courseallotment";
+import CourseConfirmation from "../components/Users/Academic/courseConfirmation";
 
-function Userslayout({ children }) {
-    console.log(children);
-    const StudentElement = children.StudentElemnt;
+function Userslayout({ UserElement }) {
+    console.log(UserElement);
+    // const StudentElement = children.StudentElemnt;
     return (
         <>
             <SideBar />
             <Routes>
-                <Route path="/fill_profile" element={<Add_per_d />} />
-                <Route path="/profile" element={<StudentElement><Profile /></StudentElement>} />
-                <Route path="/contact_details" element={<StudentElement><Student_info /></StudentElement>} />
-                <Route path="/Add_contact_details" element={<StudentElement><Add_C_d /></StudentElement>} />
-                <Route path="/Add_Educationdetails" element={<Add_EducationDetails />} />
-                <Route path="/Education_details" element={<StudentElement><Educationdetails /></StudentElement>} />
-                <Route path="/PersonalDetails" element={<StudentElement><PersonalDetails /></StudentElement>} />
-                <Route path="/addPersonalDetails" element={<StudentElement><AddPersonalDetails /></StudentElement>} />
-                <Route path="/dashboard" element={<StudentElement><Dashboard /></StudentElement>} />
-                <Route path="/coursereg" element={<StudentElement><CourseRegActivity /></StudentElement>} />
+
+                <Route path="/facultyadvisor" element={<UserElement><Facultyadvisor /></UserElement>} />
+                <Route path="/promotion" element={<UserElement><Promotion /></UserElement>} />
+                <Route path="/schemeallotment" element={<UserElement><Schemeallotment /></UserElement>} />
+                <Route path="/offeredcourses" element={<UserElement><Offeredcourses /></UserElement>} />
+                <Route path="/courseconfirm" element={<UserElement><CourseConfirmation /></UserElement>} />
+                <Route path="/newuser" element={<UserElement><NewUser /></UserElement>} />
+                <Route path="/user" element={<UserElement><UserDashboard /></UserElement>} />
+                <Route path="/batchallotment" element={<UserElement><Batchallotment /></UserElement>} />
+                <Route path="/finalcoursesoffered" element={<FinalCoursesOffered />}></Route>
+                s<Route path="/courseallotment" element={<Courseallotment />}></Route>
             </Routes>
         </>
     );
