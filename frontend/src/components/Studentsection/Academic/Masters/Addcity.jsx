8b976e2a690016cd7bfd8f81bc2b23238b914251
Rutuja Gosavi <gosavirutu523@gmail.com>
2023-05-04 22:07:16 +0530

    Academic masters ui added

--- frontend/src/components/Studentsection/Academic/Masters/Addcity.jsx
+++ frontend/src/components/Studentsection/Academic/Masters/Addcity.jsx
@@ -1,26 +1,51 @@
 import React from 'react'
-import { useState, } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Box, Button, TextField, Card, CardContent, CardHeader } from "@mui/material";
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function Addcity() {
-  const [city, setCity] = useState({
+  const [City, setCity] = useState([]);
+  const [addCity, setAddCity] = useState({
     city_id: "",
     city_name: "",
     isDistrict: "",
     state_id: "",
   });
 
+  const fetchAllCities = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/city", {
+        headers: { authorization: localStorage.getItem('token') }
+      });
+      setCity(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllCities();
+  }, []);
+
   const navigate = useNavigate();
   const handleChange = (e) => {
-    setCity((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setAddCity((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const handleClick = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:3001/city", city);
+      await axios.post("http://localhost:3001/city", addCity);
       navigate("/city");
     } catch (err) {
       console.log(err);
@@ -30,63 +55,107 @@ function Addcity() {
 
   //   console.log(branch);
   return (
-    <Box
-      component="form"
-      sx={{ "& .MuiTextField-root": { m: 2, width: "25ch" }, whiteSpace: 'normal' }}
-      noValidate
-      autoComplete="off">
+    <div style={{ height: '100%', width: '100%' }}>
+      <Box
+        component="form"
+        sx={{
+          width: '100%', height: '100%'
+        }}
+        noValidate
+        autoComplete="off">
+        <Card sx={{
+          m: 1, minWidth: 275, backgroundColor: '#F5F5F5'
+        }}>
+          <CardContent>
+            <CardHeader
+              style={{ backgroundColor: "lightblue" }}
+              title={<h1 style={{ fontSize: "30px", fontWeight: "bold", marginLeft: "10px", textAlign: 'center' }}>Add City</h1>}
+            />
+            <div style={{ padding: '5px', marginTop: 30, marginLeft: 5 }}  >
+              <Grid container spacing={1} >
+                <Grid container spacing={1} sx={{ display: 'flex', justifyContent: 'left' }}>
+                  <Grid item xs={12} sm={6} md={2} sx={{ p: 0, m: 0 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      label="City ID"
+                      name="city_id"
+                      onChange={handleChange}
+                      sx={{ m: 1, minWidth: 120, paddingLeft: 0 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={2} sx={{ p: 0, m: 0 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      name="city_name"
+                      label="City Name"
+                      onChange={handleChange}
+                      sx={{ m: 1, minWidth: 120, paddingLeft: 0 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={2} sx={{ p: 0, m: 0 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      name="isDistrict"
+                      label="Is District"
+                      onChange={handleChange}
+                      sx={{ m: 1, minWidth: 120, paddingLeft: 0 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={2} sx={{ p: 0, m: 0 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      label="State ID"
+                      name="state_id"
+                      onChange={handleChange}
+                      sx={{ m: 1, minWidth: 120, paddingLeft: 0 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={2} sx={{ p: 0, m: 0 }}>
+                    <Button variant="contained"
+                      onClick={handleClick}
+                      sx={{ ml: 1, alignSelf: 'center', mt: 1, height: 55 }} >Add</Button>
 
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <br />
 
-          <CardHeader
-            title={<h1 style={{ fontSize: "30px", fontWeight: "bold", marginLeft: "10px", textAlign: 'center' }}>Add City</h1>}
-            style={{ backgroundColor: "lightblue", padding: "1px" }}
-          />
-          <div className="form">
-            <TextField
-              required
-              type="number"
-              variant="outlined"
-              label="City ID"
-              name="city_id"
-              onChange={handleChange}
-            />
-            {/* <input type="number" placeholder="City Id" name="city_id" onChange={handleChange}/> */}
-            <TextField
-              required
-              variant="outlined"
-              name="city_name"
-              label="City Name"
-              onChange={handleChange}
-            />
-            {/* <input type="text" placeholder="city Name" name="city_name" onChange={handleChange}/> */}
-            <TextField
-              required
-              variant="outlined"
-              name="isDistrict"
-              label="Is District"
-              onChange={handleChange}
-            />
-            {/* <input type="text" placeholder="Is District " name="isDistrict" onChange={handleChange}/> */}
-            <TextField
-              required
-              type="number"
-              variant="outlined"
-              label="State ID"
-              name="state_id"
-              onChange={handleChange}
-            />
-            {/* <input type="number" placeholder="State" name="state_id" onChange={handleChange}/> */}
-            <Button variant="contained" onClick={handleClick}
-              sx={{ ml: 1, alignSelf: 'center', mt: 1, height: 55 }}>Add</Button>
-            {/* <button onClick={handleClick}>Add</button> */}
-          </div>
-        </CardContent>
-      </Card>
-    </Box>
+              <TableContainer component={Paper} style={{ marginTop: 30 }}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead style={{ backgroundColor: '#1976d2' }}>
+
+                    <TableRow>
+                      <TableCell align="center">City Id</TableCell>
+                      <TableCell align="center">City Name</TableCell>
+                      <TableCell align="center">State Id</TableCell>
+                    </TableRow>
+
+                  </TableHead>
+                  <TableBody>
+                    {City.map((city) => (
+                      <TableRow
+                        key={city.city_id} className="city"
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell align="center">{city.city_name}</TableCell>
+                        <TableCell align="center">{city.isDistrict}</TableCell>
+                        <TableCell align="center">{city.state_id}</TableCell>
+                      </TableRow>
+
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          </CardContent>
+        </Card>
+      </Box>
+    </div>
   )
 }
 
 export default Addcity
-
